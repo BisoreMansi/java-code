0f189c3dcf2658e3ad8dd429fe92c0387b626eb2
BisoreMansi <122001341+BisoreMansi@users.noreply.github.com>
2024-08-11 20:47:17 +0530

    Add files via upload

---
+++ Final/code2.java
@@ -0,0 +1,39 @@
+ class code2 
+{
+    static final int flower ;
+    static final String college = "cdgi";
+    static{
+        flower = 12;
+    }
+    final int my ;
+    code2(){
+        my =23;
+        System.out.println(my);
+    }
+  
+    public static void main(String[] args) {
+        final double PI ;
+        final String NAME ;
+       PI=3.145;
+        
+        // {
+        //     NAME = "mansi";
+
+        // }
+
+        code2 c = new code2();
+        {
+            NAME ="bhavna";
+        }
+        
+        System.out.println(flower);
+        System.out.println(PI);
+        System.out.println(NAME);
+       
+       
+
+        
+
+    }
+    
+}
